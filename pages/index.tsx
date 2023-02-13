b1389b0127f12072a52839d4004fbac1325dda5a
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-02-13 16:48:28 -0300

    Applied wrapper to _app and home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
+import type { GetStaticProps, NextPage } from 'next';
+
 import { getContentfulData } from '@/services/contentful';
 import { ISection } from '@/models/contentful/generated/contentful';
+
+import { wrapper } from '@/store';
 
 interface Props {
   response: Array<ISection>;
 }
 
-function Home({ response }: Props) {
+const Home: NextPage<Props> = ({ response }) => {
   console.log(response[0].fields.title);
   return (
     <>
       <div>Home</div>
     </>
   );
-}
-
-export const getStaticProps = async () => {
-  const response = await getContentfulData('section');
-
-  return {
-    props: {
-      response,
-    },
-  };
 };
+
+export const getStaticProps: GetStaticProps = wrapper.getStaticProps(
+  (store) => async () => {
+    const response = await getContentfulData('section');
+
+    return {
+      props: {
+        response,
+      },
+    };
+  }
+);
+
 export default Home;
