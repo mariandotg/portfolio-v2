b068c0c34341243550a7d6e5e81563ae9b0aafc8
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-02-13 16:41:39 -0300

    Created the wrapper

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createWrapper } from 'next-redux-wrapper';
 
 export const store = configureStore({
   reducer: {},
   devTools: true,
 });
 
+export const makeStore = () => {
+  return store;
+};
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof makeStore>;
+
+export const wrapper = createWrapper<AppStore>(makeStore);
