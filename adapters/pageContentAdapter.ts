0548eb9d6145ce2f125d2487de850eb3d31ab0d0
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-02-20 20:00:21 -0300

    Added pageContentAdapter

---
+++ adapters/pageContentAdapter.ts
@@ -0,0 +1,33 @@
+import { ISection } from '@/models/contentful/generated/contentful';
+import { Input } from '@/models/domain/Input';
+import { JobCard } from '@/models/domain/JobCards';
+import { SkillCard } from '@/models/domain/SkillCard';
+import { PageContentSections } from '@/models/store/state/PageContentSections';
+
+export function pageContentAdapter(data: Array<ISection>): PageContentSections {
+  return data.reduce<PageContentSections>((acc, section) => {
+    const { slug, ...sectionFields } = section.fields;
+
+    const content = sectionFields.content!.reduce((acc, contentObj) => {
+      const { slug, ...contentFields } = contentObj.fields;
+
+      if ('content' in contentFields) {
+        const content = contentFields.content.reduce<
+          Array<JobCard | SkillCard | Input>
+        >((acc, content) => {
+          const { slug, ...fields } = content?.fields;
+          const id = content?.sys.id;
+
+          return [...acc, { ...fields, id }];
+        }, []);
+
+        return { ...acc, [slug]: content };
+      }
+
+      return { ...acc, [slug]: contentFields };
+    }, {});
+
+    const fields = { ...sectionFields, content };
+    return { ...acc, [slug]: fields };
+  }, {} as PageContentSections);
+}
