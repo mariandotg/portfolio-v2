68f60aaf1d28451e62379f331ea7fb157f322a67
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-02-20 19:56:47 -0300

    Added PageContentSections model

---
+++ models/store/state/PageContentSections.ts
@@ -0,0 +1,18 @@
+import {
+  AboutContent,
+  ProjectsContent,
+  SkillsContent,
+  JobsContent,
+  ArticlesContent,
+  ContactContent,
+} from '@/models/domain/FormattedData/FormattedContent';
+import { FormattedSection } from '@/models/domain/FormattedData/FormattedSection';
+
+export interface PageContentSections {
+  about: FormattedSection<AboutContent>;
+  featuredProjects: FormattedSection<ProjectsContent>;
+  skills: FormattedSection<SkillsContent>;
+  jobExperience: FormattedSection<JobsContent>;
+  latestArticles: FormattedSection<ArticlesContent>;
+  contact: FormattedSection<ContactContent>;
+}
