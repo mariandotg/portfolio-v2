355ec8a9e0895b6b856b03197d51b09ad7035f27
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-02-20 20:01:06 -0300

    Added PageConstantsConstants model

---
+++ models/store/state/PageConstantsConstants.ts
@@ -0,0 +1,7 @@
+import { SocialMedia } from '@/models/domain/SocialMedia';
+import { Text } from '@/models/domain/Text';
+
+export interface PageConstantsConstants {
+  text: Text;
+  social: Array<SocialMedia>;
+}
