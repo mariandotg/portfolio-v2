3a153a50b699229fffdb95fac646354fa570cd4b
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-02-23 07:27:37 -0300

    Added notion service and notion env variables

---
+++ services/notion/index.ts
@@ -0,0 +1,17 @@
+const { Client } = require('@notionhq/client');
+
+const client = new Client({
+  auth: process.env.NEXT_PUBLIC_NOTION_AUTH_TOKEN,
+});
+
+export const queryNotionDatabase = async ({
+  databaseId,
+  filter,
+}: any): Promise<Array<any>> => {
+  const response = await client.databases.query({
+    database_id: databaseId,
+    filter,
+  });
+
+  return response;
+};
