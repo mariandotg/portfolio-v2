7e54f89e29b8e08a80891f15a9bb5d96e95bf78a
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-03-02 00:29:46 -0300

    Added basic themes colors and setted darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -10,6 +11,15 @@ module.exports = {
       display: ['IBM Plex Sans', 'system-ui', 'sans-serif'],
       body: ['IBM Plex Sans', 'system-ui', 'sans-serif'],
       monospace: ['IBM Plex Mono', 'system-ui', 'monospace'],
+    },
+    colors: {
+      dark: '#0D0D0D',
+      "dark-headlines": "#E1E1E1",
+      "dark-text": "#B1B1B1",
+      light: '#F8F8FA',
+      "light-headlines": "#0D0D0D",
+      "light-text": "#5E5E5E",
+      primary: '#7050D8',
     },
   },
   plugins: [],
