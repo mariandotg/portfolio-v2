c92c0b4e812f751df80977b9847fe78378e86d56
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-03-09 21:11:14 -0300

    Added about and featuredProjects base sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,44 @@ const Home: NextPage = () => {
   console.log(response.about);
   return (
     <PageLayout>
-      <section className='grid grid-cols-3 gap-y-8'>hola</section>
+      <section>
+        <div className='flex flex-col col-span-3 gap-y-4'>
+          <h1 className='flex justify-center italic font-medium text-section-title text-light-headlines font-monospace'>
+            {response.about.title}
+          </h1>
+          <p className='font-display text'>
+            {response.about.content.ctaDescription.text}
+          </p>
+        </div>
+        <button className='col-span-3 py-3 italic font-bold rounded mobile:col-span-1 mobile:col-end-4 bg-primary font-monospace text-light'>
+          {response.about.content.cta.label}
+        </button>
+      </section>
+      <section>
+        <h2 className='italic font-medium text-section-title font-monospace text-light-headlines'>
+          {response.featuredProjects.title}
+        </h2>
+        <div className='flex flex-col gap-y-8'>
+          {response.featuredProjects.content.projects.map((p) => {
+            return (
+              <div key={p.id} className='flex flex-col gap-y-2 text'>
+                <img
+                  src={p.image}
+                  className='object-cover w-full rounded h-[187px]'
+                />
+                <h3 className='font-medium text-title font-display text-light-headlines'>
+                  {p.name}
+                </h3>
+                <p className='text-light-text'>{p.description}</p>
+                <span className='font-medium '>{p.tags[0].name}</span>
+              </div>
+            );
+          })}
+        </div>
+        <button className='w-full px-6 py-3 italic font-bold text-center rounded bg-light-text text-light font-monospace'>
+          View more
+        </button>
+      </section>
     </PageLayout>
   );
 };
