11a3a8e5c66a11c0ea82fe7d81b6376beb09caea
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-03-09 21:35:11 -0300

    Added About component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import type { GetStaticProps, NextPage } from 'next';
 
 import { wrapper } from '@/store';
-import { useAppSelector } from '@/hooks/store/useAppSelector';
 
 import { fetchPageContent } from '@/store/actions/pageContent/fetchPageContent';
 import { fetchNotionContent } from '@/store/actions/pageContent/fetchNotionContent';
@@ -10,27 +9,13 @@ import { fetchPageConstants } from '@/store/actions/pageConstants/fetchPageConst
 import { fetchNotionSeo } from '@/store/actions/pageSeo/fetchNotionSeo';
 
 import PageLayout from '@/components/PageLayout';
-import Section from '@/components/Section';
+import About from '@/components/About';
 import FeaturedProjects from '@/components/FeaturedProjects';
 
 const Home: NextPage = () => {
-  const response = useAppSelector((state) => state.pageContent.sections);
-  console.log(response.about);
   return (
     <PageLayout>
-      <Section>
-        <div className='flex flex-col col-span-3 gap-y-4'>
-          <h1 className='flex justify-center italic font-medium text-section-title text-light-headlines font-monospace'>
-            {response.about.title}
-          </h1>
-          <p className='font-display text'>
-            {response.about.content.ctaDescription.text}
-          </p>
-        </div>
-        <button className='col-span-3 py-3 italic font-bold rounded mobile:col-span-1 mobile:col-end-4 bg-primary font-monospace text-light'>
-          {response.about.content.cta.label}
-        </button>
-      </Section>
+      <About />
       <FeaturedProjects />
     </PageLayout>
   );
