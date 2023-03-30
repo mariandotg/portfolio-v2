5644d0ae332d1c6a25721e04298c187f14540a64
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-03-29 22:52:40 -0300

    Modified ProjectCard tags

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -23,38 +23,10 @@ const ProjectCard = (props: Props) => {
       props.onHover(props.project);
     }
   } */
-  const contenedorRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [restTags, setRestTags] = useState(0);
   const [numMaxTags, setNumMaxTags] = useState(0);
-  const [tagsMostradas, setTagsMostradas] = useState<Tag[]>([]);
-
-  useEffect(() => {
-    const contenedor = contenedorRef.current;
-    if (!contenedor) return;
-    const contenedorAncho = contenedor.getBoundingClientRect().width;
-    const tagsRefs = Array.from(contenedor.children).map(
-      (child) => child.getBoundingClientRect().width
-    );
-    let anchoActual = 0;
-    let numMaxTags = 0;
-    tagsRefs.some((tagRef) => {
-      anchoActual += tagRef;
-      numMaxTags++;
-      return anchoActual >= contenedorAncho;
-    });
-    setNumMaxTags(numMaxTags);
-  }, []);
-
-  useEffect(() => {
-    setTagsMostradas(props.project.tags.slice(0, numMaxTags));
-  }, [props.project.tags, numMaxTags]);
-
-  console.log(tagsMostradas);
-  function mostrarMasTags() {
-    const numTagsRestantes = props.project.tags.length - numMaxTags;
-    const tagsRestantes = props.project.tags.slice(numMaxTags);
-    setTagsMostradas([...tagsMostradas, ...tagsRestantes]);
-    setNumMaxTags(numMaxTags + numTagsRestantes);
-  }
+  const [displayedTags, setDisplayedTags] = useState<Tag[]>(props.project.tags);
 
   function acortarString(str: string): string {
     const maxLength = 27;
@@ -63,6 +35,75 @@ const ProjectCard = (props: Props) => {
     }
     return str;
   }
+
+  useEffect(() => {
+    const container = containerRef.current!;
+    const containerWidth = container.getBoundingClientRect().width;
+    const tagsWidths = Array.from(container.children).map(
+      (child) => child.getBoundingClientRect().width
+    );
+    let actualWidth = 24;
+    let numMaxTags = 0;
+    tagsWidths.forEach((tagRef) => {
+      if (actualWidth + tagRef <= containerWidth) {
+        actualWidth += tagRef;
+        numMaxTags++;
+      }
+    });
+    setNumMaxTags(numMaxTags);
+  }, [props.project.tags, containerRef, setNumMaxTags]);
+
+  useEffect(() => {
+    const container = containerRef.current!;
+    const containerWidth = container.getBoundingClientRect().width;
+    const tagsWidths = Array.from(container.children).map(
+      (child) => child.getBoundingClientRect().width
+    );
+    let actualWidth = 24;
+    let numMaxTags = 0;
+    tagsWidths.forEach((tagRef) => {
+      if (actualWidth + tagRef <= containerWidth) {
+        actualWidth += tagRef;
+        numMaxTags++;
+      }
+    });
+    setNumMaxTags(numMaxTags);
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.target === containerRef.current) {
+          const containerWidth = entry.target.getBoundingClientRect().width;
+          let actualWidth = 24;
+          let numMaxTags = 0;
+          tagsWidths.forEach((tagRef) => {
+            if (actualWidth + tagRef <= containerWidth) {
+              actualWidth += tagRef;
+              numMaxTags++;
+            }
+          });
+          setNumMaxTags(numMaxTags);
+        }
+      }
+    });
+
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [containerRef, setNumMaxTags]);
+
+  useEffect(() => {
+    setDisplayedTags(props.project.tags.slice(0, numMaxTags));
+    setRestTags(props.project.tags.length - numMaxTags);
+  }, [props.project.tags, numMaxTags]);
+
+  console.log(props.project.name);
+  console.log(props.project.tags.length);
+  console.log(numMaxTags);
+
   return (
     <div
       className={`flex flex-col cursor-pointer gap-y-2 group tablet:p-4 tablet:border-[1px] rounded ${
@@ -87,17 +128,17 @@ const ProjectCard = (props: Props) => {
             {acortarString(props.project.description)}
           </p>
         </div>
-        <div className='flex flex-row gap-x-4' ref={contenedorRef}>
-          {tagsMostradas.map((tag) => (
+        <div
+          className='flex flex-row items-center w-full gap-x-4'
+          ref={containerRef}
+        >
+          {displayedTags.map((tag, index) => (
             <SkillItem key={tag.id} skill={tag.name} />
           ))}
-          {props.project.tags.length > numMaxTags && (
-            <button
-              onClick={mostrarMasTags}
-              className='dark:text-dark-headlines text-light-headlines'
-            >
-              +{props.project.tags.length - numMaxTags} más
-            </button>
+          {restTags !== 0 && (
+            <span className='h-[20px] w-[20px] flex justify-center items-center rounded-[50px] bg-light-text dark:bg-dark-text text-dark-headlines dark:text-light-headlines font-medium text-[12px]'>
+              +{restTags}
+            </span>
           )}
         </div>
       </div>
