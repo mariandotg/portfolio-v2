fcf4cfffb29c3dd424b212f3db167b48eb69d274
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-09 18:56:45 -0300

    Modified ProjectCard component

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -4,7 +4,6 @@ import { Project } from '@/models/domain/Project';
 import { MdArrowForward } from 'react-icons/md';
 import SkillItem from '../Skills/SkillItem';
 import { Tag } from '@/models/domain/Tag';
-import { useAppSelector } from '@/hooks/store/useAppSelector';
 
 interface Props {
   project: Project;
@@ -14,6 +13,14 @@ interface Props {
 }
 
 const ProjectCard = (props: Props) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [restTags, setRestTags] = useState(0);
+  const [numMaxTags, setNumMaxTags] = useState(0);
+  const [displayedTags, setDisplayedTags] = useState<Tag[]>(props.project.tags);
+  const [hovered, setHovered] = useState(
+    props.project.id === props.currentProject.id
+  );
+
   function acortarString(str: string): string {
     const maxLength = 27;
     if (str.length > maxLength) {
@@ -22,6 +29,78 @@ const ProjectCard = (props: Props) => {
     return str;
   }
 
+  useEffect(() => {
+    const container = containerRef.current!;
+    const containerWidth = container.getBoundingClientRect().width;
+    const tagsWidths = Array.from(container.children).map(
+      (child) => child.getBoundingClientRect().width
+    );
+    let actualWidth = 24;
+    let numMaxTags = 0;
+    tagsWidths.forEach((tagRef) => {
+      if (actualWidth + tagRef <= containerWidth) {
+        actualWidth += tagRef;
+        numMaxTags++;
+      }
+    });
+    setNumMaxTags(numMaxTags);
+  }, [props.project.tags, containerRef, setNumMaxTags]);
+
+  useEffect(() => {
+    const container = containerRef.current!;
+    const containerWidth = container.getBoundingClientRect().width;
+    const tagsWidths = Array.from(container.children).map(
+      (child) => child.getBoundingClientRect().width
+    );
+    let actualWidth = 24;
+    let numMaxTags = 0;
+    tagsWidths.forEach((tagRef) => {
+      if (actualWidth + tagRef <= containerWidth) {
+        actualWidth += tagRef;
+        numMaxTags++;
+      }
+    });
+    setNumMaxTags(numMaxTags);
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.target === containerRef.current) {
+          const containerWidth = entry.target.getBoundingClientRect().width;
+          let actualWidth = 24;
+          let numMaxTags = 0;
+          tagsWidths.forEach((tagRef) => {
+            if (actualWidth + tagRef <= containerWidth) {
+              actualWidth += tagRef;
+              numMaxTags++;
+            }
+          });
+          setNumMaxTags(numMaxTags);
+        }
+      }
+    });
+
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [containerRef, setNumMaxTags]);
+
+  useEffect(() => {
+    setDisplayedTags(props.project.tags.slice(0, numMaxTags));
+    setRestTags(props.project.tags.length - numMaxTags);
+  }, [props.project.tags, numMaxTags]);
+
+  if (props.onHover) {
+    if (hovered) {
+      props.onHover(props.project);
+    }
+  }
+  console.log(props.project.name);
+  console.log(props.project.tags.length);
+  console.log(numMaxTags);
   return (
     <div
       className={`flex flex-col tablet:col-span-1 ${
@@ -29,6 +108,8 @@ const ProjectCard = (props: Props) => {
           ? 'bg-light-primary-hover/10 dark:bg-dark-primary-hover/10 dark:border-primary border-primary dark:text-dark-text text-light-text'
           : 'hover:bg-light-headlines/5 dark:hover:bg-dark-headlines/5 tablet:border-transparent dark:hover:text-dark-text hover:text-light-text dark:border-transparent border-transparent tablet:hover:border-light-headlines/25 tablet:dark:hover:border-dark-headlines/25'
       } cursor-pointer gap-y-4 group tablet:p-4 tablet:border rounded dark:text-dark-text text-light-text`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
     >
       <div className='flex flex-col gap-y-2'>
         <img
@@ -43,10 +124,18 @@ const ProjectCard = (props: Props) => {
           <p className='text'>{acortarString(props.project.description)}</p>
         </div>
       </div>
-      <div className='flex flex-row items-center w-full gap-x-4'>
-        {props.project.tags.map((tag, index) => (
+      <div
+        className='flex flex-row items-center w-full gap-x-4'
+        ref={containerRef}
+      >
+        {displayedTags.map((tag, index) => (
           <SkillItem key={tag.id} skill={tag.name} />
         ))}
+        {restTags !== 0 && (
+          <span className='h-[20px] w-[20px] flex justify-center items-center rounded-[50px] bg-light-text dark:bg-dark-text text-dark-headlines dark:text-light-headlines font-medium text-[12px]'>
+            +{restTags}
+          </span>
+        )}
       </div>
     </div>
   );
