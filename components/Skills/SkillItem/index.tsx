6812a1bc0b02e86fc764fb1be8afe1531f3eeded
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-09 09:02:58 -0300

    Added white space no wrap to SkillItem

--- components/Skills/SkillItem/index.tsx
+++ components/Skills/SkillItem/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const SkillItem = ({ skill }: Props) => {
   return (
-    <span className='relative flex hover:after:bg-primary/40 hover:after:brightness-150 hover:after:blur-[8px] hover:after:content-[""] hover:after:absolute hover:after:h-[12px] hover:after:w-[12px] hover:after:z-[999] items-center text-light-text dark:text-dark-text group/item gap-x-2'>
+    <span className='relative whitespace-nowrap flex text hover:after:bg-primary/40 hover:after:brightness-150 hover:after:blur-[8px] hover:after:content-[""] hover:after:absolute hover:after:h-[12px] hover:after:w-[12px] hover:after:z-[999] items-center text-light-text dark:text-dark-text group/item gap-x-2'>
       <Icon
         value={skill.toLocaleLowerCase()}
         className='duration-[0ms] group-hover/item:fill-primary fill-light-text dark:fill-dark-text'
