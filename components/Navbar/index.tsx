f8e3ff575db7f69e80844e0163c89c9365b6e79d
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-12 19:08:25 -0300

    Fixed brand logo disabling navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -22,19 +22,20 @@ const Navbar = () => {
 
   return (
     <nav className='border-b-[1px] border-primary bg-light/80 z-[9999] dark:bg-dark/70 backdrop-saturate-200 fixed top-0 flex justify-center w-full px-4 py-3 backdrop-blur'>
-      <div className='absolute top-0 flex justify-center w-full h-[57px]'>
-        <img
-          src='./mlogolight.svg'
-          className='hidden tablet:dark:flex tablet:flex tablet:dark:brightness-[200] w-[164px]'
-        />
-        <img src='./mdg_logo.svg' className='flex tablet:hidden w-[50px]' />
-      </div>
-      <div className='flex items-center w-screen tablet:max-w-[800px] justify-between gap-16'>
+      <div className='relative flex items-center w-screen tablet:max-w-[800px] justify-between gap-16'>
         <div className='items-center hidden gap-4 tablet:flex dark:text-light'>
           <Navlink href='/'>Portfolio</Navlink>
           <Navlink href='/projects'>Proyectos</Navlink>
           <Navlink href='/blog'>Blog</Navlink>
         </div>
+        <img
+          src='./mlogolight.svg'
+          className='absolute left-2/4 -translate-x-1/2 hidden tablet:dark:flex tablet:flex tablet:dark:brightness-[200] w-[164px]'
+        />
+        <img
+          src='./mdg_logo.svg'
+          className='absolute left-2/4 -translate-x-1/2  flex tablet:hidden w-[50px]'
+        />
         <Button variant='secondary' className='flex tablet:hidden' icon>
           <MdMenu className='duration-[0ms] w-[18px] h-[18px]' />
         </Button>
